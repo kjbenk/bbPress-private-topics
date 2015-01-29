b53bdffb437c8aa201d8bc0327bb3662d62e6e09
Kyle <kjbenk@gmail.com>
2015-01-29 13:15:33 -0500

    Added Features
    * Create forum upon user subscribing to membership
    * Limit number of topics created by a user when purchasing single tasks

--- bbpress-private-topics.php
+++ bbpress-private-topics.php
@@ -47,12 +47,13 @@ function wps_check_topics_in_progress( $forum_id ) {
 
 	$user_id = get_current_user_id();
 
-	if ($user_id) {
+	if ( !current_user_can('manage_options') && $user_id && $forum_id != 19) {
 
 		// Get all topics that are in progress
 
 		$topics_in_progress = get_posts( array(
 			'post_type'      => 'topic',
+			'post_parent'	 => $forum_id,
 			'author'		 => $user_id,
 			'post_status' 	 => array( 'publish', 'pending', 'open' ),
 			'posts_per_page' => -1
@@ -68,5 +69,148 @@ function wps_check_topics_in_progress( $forum_id ) {
 		}
 
 	}
+
+}
+
+add_action( 'bbp_new_topic_pre_extras', 'wps_check_single_topic_count' );
+
+/**
+ * Return an Error message if user tries to create more topics then
+ *  the number of single topics they have purchased
+ *
+ * @access public
+ * @param mixed $post_id
+ * @return void
+ */
+function wps_check_single_topic_count( $forum_id ) {
+
+	// Get current user ID
+
+	$user_id = get_current_user_id();
+
+	if ( $user_id && $forum_id == 19 ) {
+
+		// Get all topics that are in progress
+
+		$topics = get_posts( array(
+			'post_type'      	=> 'topic',
+			'post_parent'	 	=> $forum_id,
+			'author'		 	=> $user_id,
+			'posts_per_page' 	=> -1
+		) );
+
+		$orders = get_posts( array(
+			'post_type'			=> 'shop_order',
+			'post_status'		=> 'wc-completed',
+			'author'		 	=> $user_id,
+			'posts_per_page' 	=> -1
+		) );
+
+		error_log(count($orders));
+
+		global $wpdb;
+
+		$order_quantity = 0;
+
+		foreach ($orders as $order) {
+
+			$item = $wpdb->get_results(
+				$wpdb->prepare(
+					'SELECT * FROM `' . $wpdb->prefix . 'woocommerce_order_items` WHERE `order_id` = %d AND `order_item_type` = %s',
+					$order->ID,
+					'line_item'
+				)
+			);
+
+			$quantity = $wpdb->get_results(
+				$wpdb->prepare(
+					'SELECT * FROM `' . $wpdb->prefix . 'woocommerce_order_itemmeta` WHERE `order_item_id` = %d AND `meta_key` = %s',
+					$item[0]->order_item_id,
+					'_qty'
+				)
+			);
+
+			$order_quantity += $quantity[0]->meta_value;
+
+		}
+
+		error_log($order_quantity);
+
+		// If there is a topic in progress then thrown an error
+
+		if (count($topics) >= $order_quantity) {
+
+			bbp_add_error( 'bbp_topic_error', __( '<strong>ERROR</strong>: You must purchase another single task before you can create another support ticket.  If you have already purchased another tasks your order might still be processing, so please bear with us.', 'bbpress' ) );
+
+			return;
+		}
+
+	}
+
+}
+
+add_action( 'woocommerce_add_order_item_meta', 'wps_add_forum', 10, 3);
+
+/**
+ * Add a new forum when user completes an order
+ *
+ * @access public
+ * @param mixed $order_id
+ * @return void
+ */
+function wps_add_forum( $item_id, $values, $cart_item_key ) {
+
+	// Get the domain name form the post meta data
+
+	foreach ($values['addons'] as $addons) {
+
+		$doamin_name = $addons['value'];
+
+	}
+
+	$topic_data = array(
+		'post_title'    => $doamin_name,
+	);
+
+	// Gold Membership
+
+	if ($values['data']->post->ID == 8) {
+
+		// Create a new topic from the domain name
+
+		$topic_id = bbp_insert_forum( $topic_data );
+
+		update_post_meta($topic_id, 'groups-groups_read_post', 'gold membership');
+
+	}
+
+	// Silver Membership
+
+	else if ($values['data']->post->ID == 9) {
+
+		// Create a new topic from the domain name
+
+		$topic_id = bbp_insert_forum( $topic_data );
+
+		update_post_meta($topic_id, 'groups-groups_read_post', 'silver membership');
+
+	}
+
+}
+
+add_filter( 'woocommerce_new_order_data', 'wps_change_post_author_for_order');
+
+/**
+ * Change the post author to the current user when an order is created
+ *
+ * @access public
+ * @param mixed $order_data
+ * @return void
+ */
+function wps_change_post_author_for_order( $order_data ) {
+
+	$order_data['post_author'] = get_current_user_id();
+
+	return $order_data;
 
 }
